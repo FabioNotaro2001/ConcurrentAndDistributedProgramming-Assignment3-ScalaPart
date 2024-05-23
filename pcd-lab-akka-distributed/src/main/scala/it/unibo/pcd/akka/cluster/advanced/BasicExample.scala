af8bddc0d058b371b9f7113b29457214a7919da4
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-05-23 22:21:59 +0200

    Go on

--- pcd-lab-akka-distributed/src/main/scala/it/unibo/pcd/akka/cluster/advanced/BasicExample.scala
+++ pcd-lab-akka-distributed/src/main/scala/it/unibo/pcd/akka/cluster/advanced/BasicExample.scala
@@ -1,11 +1,11 @@
 package it.unibo.pcd.akka.cluster.advanced
-
+ 
 import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.*
 import akka.cluster.typed.Cluster
 import com.typesafe.config.ConfigFactory
 import it.unibo.pcd.akka.cluster.seeds
-
+ 
 import concurrent.duration.{DurationInt, FiniteDuration}
 import scala.language.postfixOps
 import scala.util.Random
@@ -22,12 +22,12 @@ object Root:
       ctx.spawnAnonymous(LobbyActor.apply())
     Behaviors.empty
   }
-
+ 
 @main def startLobby: Unit =
   startupWithRole(Roles.backend, 2551)(Root(""))
-
+ 
 @main def startPlayer1: Unit =
   startupWithRole(Roles.frontend, 0)(Root("P1"))
-
+ 
 @main def startPlayer2: Unit =
   startupWithRole(Roles.frontend, 0)(Root("P2"))
--- pcd-lab-akka-distributed/src/main/scala/it/unibo/pcd/akka/cluster/advanced/LobbyActor.scala
+++ pcd-lab-akka-distributed/src/main/scala/it/unibo/pcd/akka/cluster/advanced/LobbyActor.scala
@@ -7,25 +7,35 @@ import it.unibo.pcd.akka.cluster.advanced.messages.{CreateGame, JoinGame, Sudoku
 import scala.collection.mutable.Map as MutableMap
 import messages.*
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-
+ 
+ 
+private case class aListingResponse(listing: Receptionist.Listing)
+ 
 object LobbyActor:
-  val lobbyKey: ServiceKey[LobbyMessage] = ServiceKey("lobby")
-
-  def apply(): Behavior[LobbyMessage] =
+  val lobbyKey: ServiceKey[LobbyMessage | aListingResponse] = ServiceKey("lobby")
+ 
+  def apply(): Behavior[LobbyMessage | aListingResponse] =
     Behaviors.setup(context => LobbyActor(context))
-
-  private case class LobbyActor(override val context: ActorContext[LobbyMessage]) extends AbstractBehavior[LobbyMessage](context):
+ 
+  private case class LobbyActor(override val context: ActorContext[LobbyMessage | aListingResponse]) extends AbstractBehavior[LobbyMessage | aListingResponse](context):
     private val players = MutableMap[String, ActorRef[PlayerMessage]]()
-
+   
+    private val listingResponseAdapter = context.messageAdapter[Receptionist.Listing](aListingResponse.apply)
+    context.system.receptionist ! Receptionist.Subscribe(lobbyKey, listingResponseAdapter)
     context.system.receptionist ! Receptionist.Register(lobbyKey, context.self)
-
-    override def onMessage(message: LobbyMessage): Behavior[LobbyMessage] = message match
-      case CreateGame(id, player) =>
-        players.put(id, player)
-        Behaviors.same
-      case EndGame(id) =>
-        players.remove(id)
-        Behaviors.same
-      case FindGames(receiver) =>
-        receiver ! Games(players.values.toList)
-        Behaviors.same
+ 
+    override def onMessage(message: LobbyMessage | aListingResponse): Behavior[LobbyMessage| aListingResponse] =
+      println("ENTRATOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO " + message)
+      message match
+        case aListingResponse(contents) =>
+          println("CONTENUTOOOOOOOOOOOOOOOOOOO LOBBYYYYYYYYYYYYYYYYYY" + contents)
+          Behaviors.same
+        case CreateGame(id, player) =>
+          players.put(id, player)
+          Behaviors.same
+        case EndGame(id) =>
+          players.remove(id)
+          Behaviors.same
+        case FindGames(receiver) =>
+          receiver ! Games(players.values.toList)
+          Behaviors.same
